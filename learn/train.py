b2a934c9a41bc64552e1b24b1c28611513baf7c3
beomjoon <beomjoon@mit.edu>
2019-06-27 12:16:43 -0400

    updating gitignore

--- learn/train.py
+++ learn/train.py
@@ -69,7 +69,6 @@ def train(config):
     nodes, edges, actions, costs = data_traj.load_data(
         './test_results/hpn_results_on_mover_domain/1/trajectory_data//',
         desired_operator_type=config.operator)
-    import pdb;pdb.set_trace()
     num_training = min(config.num_train, len(nodes))
     num_test = min(config.num_test, len(nodes))
     config.num_train = num_training
