7366ba4f9221fa309b5508f17b8a89b97d1a991f
beomjoon <beomjoon@mit.edu>
2019-06-26 21:36:58 -0400

    pidx option

--- test_scripts/threaded_test_rsc.py
+++ test_scripts/threaded_test_rsc.py
@@ -26,10 +26,14 @@ def main():
     parser.add_argument('-n_objs_pack', type=int, default=2)
     parser.add_argument('-seed', type=int, default=0)
     parser.add_argument('-time_limit', type=int, default=1000)
+    parser.add_argument('-pidxs', nargs='+', type=int)
     parameters = parser.parse_args()
 
+    pidx_begin = parameters.pidxs[0]
+    pidx_end = parameters.pidxs[1]
+
     configs = []
-    for pidx in range(2000, 2100):
+    for pidx in range(pidx_begin, pidx_end):
         config = {'problem_idx': pidx,
                   'seed': parameters.seed,
                   'n_objs_pack': parameters.n_objs_pack,
