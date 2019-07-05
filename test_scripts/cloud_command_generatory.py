42f1aecc2ff7d5b93a8128d6f5131d43638c252c
Beomjoon Kim <beomjoon@mit.edu>
2019-07-05 06:39:11 -0400

    test committing to my branch

--- test_scripts/cloud_command_generatory.py
+++ test_scripts/cloud_command_generatory.py
@@ -17,20 +17,20 @@ else:
 
 command = "cd /root/qqq ; git pull;  python test_scripts/threaded_test_%s.py" % algorithm
 if algorithm != 'hpn' and algorithm != 'greedy_no_gnn':
-    train_seed = [1, 2, 3]
+    train_seed = [1, 4]
     command += " -domain %s -loss %s -num_train %d -n_objs_pack %d -time_limit %d " % (
         domain, loss, num_train, n_objs_pack, time_limit)
 else:
-    train_seed = [0]
+    train_seed = [0, 1]
     command += " -domain %s -n_objs_pack %d -time_limit %d " % (
         domain, n_objs_pack, time_limit)
 
 
-print "Total instances needed", len(train_seed) * len(planning_seed) * len(pidxs)
+print "Total instances needed", len(planning_seed) * len(pidxs)
 commands = []
-for tseed in train_seed:
-    for pseed in planning_seed:
-        for pidx in pidxs:
-            new_command = command + "-planner_seed %d -train_seed %d -pidxs %d %d" % (pseed, tseed, pidx[0], pidx[1])
-            print new_command
-            raw_input('continue?')
+#for tseed in train_seed:
+for pseed in planning_seed:
+    for pidx in pidxs:
+        new_command = command + "-planner_seed %d -train_seed %d %d -pidxs %d %d" % (pseed, train_seed[0], train_seed[1], pidx[0], pidx[1])
+        print new_command
+        raw_input('continue?')
